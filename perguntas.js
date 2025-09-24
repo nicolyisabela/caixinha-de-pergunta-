01e057d5ed8da3f0772fbdd48a3f95ba14074fc8
nicolyisabela <mendonca.santos.nicoly@escola.pr.gov.br>
2025-09-24 10:43:56 -0300

    ..

--- perguntas.js
+++ perguntas.js
@@ -11,20 +11,20 @@ criacartao(
 criacartao(
     'Tudo sobre Dog',
     ' Quando levar filhote no veterinário?',
-     Os pets filhotes precisam ir regularmente ao veterinário todos os meses pelo menos até os seus 6
-     meses de vida, para que o seu crescimento e desenvolvimento possam ser observados por um profissional.
+    'Os pets filhotes precisam ir regularmente ao veterinário todos os meses pelo menos até os seus 6 meses de vida, para que o seu crescimento e desenvolvimento possam ser observados por um profissional.'
 )
 criacartao(
     'tudo sobre volei',
     ' O que o vôlei exige?',
-    ' Para jogar vôlei, você precisa, no mínimo, de algumas pessoas, uma bola, uma rede e uma quadra. Para o
-            conforto e segurança do atleta, é essencial usar um uniforme adequado (shorts, camiseta, meias), tênis
-            específicos para o esporte com boa aderência.'
+    ' Para jogar vôlei, você precisa, no mínimo, de algumas pessoas, uma bola, uma rede e uma quadra. Para o conforto e segurança do atleta, é essencial usar um uniforme adequado (shorts, camiseta, meias), tênis específicos para o esporte com boa aderência.'
 )
 criacartao(
     'tudo sobre volei',
-     ' Quais são as 4 regras do vôlei?'
-     '  As 4 regras básicas do voleibol são: um jogo é disputado em sets até vencer 3, cada set tem 25 pontos (ou 15
-            no tie-break) com diferença de 2 pontos, a equipa só pode tocar na bola 3 vezes antes de enviá-la para o
-            campo adversário, e um jogador não pode tocar na bola 2 vezes seguidas.',
+     'Quais são as 4 regras do vôlei?'
+     'As 4 regras básicas do voleibol são: um jogo é disputado em sets até vencer 3, cada set tem 25 pontos (ou 15 no tie-break) com diferença de 2 pontos, a equipa só pode tocar na bola 3 vezes antes de enviá-la para o campo adversário, e um jogador não pode tocar na bola 2 vezes seguidas.'
+)
+criacartao(
+    'Tudo sobre volei',
+    'Qual país é o país do vôlei?'
+    ' O vôlei foi criado em 9 de fevereiro de 1895 por William George Morgan nos Estados Unidos.'
 )
