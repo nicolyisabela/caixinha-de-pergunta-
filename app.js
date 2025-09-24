4af06b2f9f8d6a4c6d5ae4ecbbfe8ced3f1aa445
nicolyisabela <mendonca.santos.nicoly@escola.pr.gov.br>
2025-09-24 11:05:17 -0300

    .....

---
+++ app.js
@@ -0,0 +1,16 @@
+function criarCartao(categoria, pergunta, resposta) {
+   let contener = document.getElementById('container')
+   let cartao = document.createElement('article')
+   cartao.className = 'cartao'
+cartao.innerHTML = 
+     <div class="cartao_conteudo">
+          <h3>Tudo sobre Dog</h3>
+          <div class="cartao_conteudo_perguntas">
+            Quais perguntas devo fazer ao veterinário na primeira consulta?
+          </div>
+          <div class="cartao_conteudo_respostas">
+            Na primeira consulta veterinária, pergunte sobre a saúde geral do seu animal, o calendário de vacinação e
+            desparasitação, o protocolo de prevenção de pulgas e carrapatos, e a melhor dieta para o pet.
+          </div>
+        </div>
+}
